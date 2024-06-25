7066e1f5d133103e7ce24340503ddf33d1ca0e79
Paweł D <you@example.com>
2024-06-25 17:22:05 +0200

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // Wyczyść katalog output przed każdym buildem
   },
-  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
 };
